830f939b8052c1e728e4a719c07853de279669ad
Racso1964 <oscarrodolfoeo@gmail.com>
2025-05-18 17:20:30 -0400

    Estilo: Comenta un bloque de lineas con /* y cierra el bloque con */

--- c.cpp
+++ c.cpp
@@ -6,11 +6,11 @@ using namespace std;
 
 int main()
 {
-    cout<<"Hola, mundo! "<<endl;
+    /*cout<<"Hola, mundo! "<<endl;
     cout<<"BIENVENIDOS"<<endl;
     cout<<"a mi proyecto: CONOCIENDO C PLUS PLUS"<<endl;
 
-    cout<<"La suma de 5 + 5 es: "<<5+5<<endl;
+    */cout<<"La suma de 5 + 5 es: "<<5+5<<endl;
     cout<<"La operacion de un promedio es: "<<(8+10+6)/3<<endl;
 
     cout<<"Hola mundo!\nBIENVENIDOS \n A mi proyecto CONOCIENDO C PLUS PLUS\n\t\t\t    con RACSO";
