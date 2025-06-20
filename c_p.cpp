25aeda7e8fa076daa80678881fa8c9a9e04c408b
Racso1964 <oscarrodolfoeo@gmail.com>
2025-06-20 11:27:30 -0400

    Despliega texto y variale

--- c_p.cpp
+++ c_p.cpp
@@ -4,9 +4,33 @@ using namespace std;
 
 int main()
 {
-    int x;
-    int y;
+    //DECLARACION DE VARIABLE
+    int x,y;
     char letra;
-    float promedio;
+    float calificacion1, calificacion2, promedio;
+    string nombre;
+
+    //ASIGNACION DE VARIABLE
+    x=10;
+    y=20;
+    letra='a'; 
+    //VISUALIAR VALOR DE VARIABLE
+    cout<<"El valor de x es: "<<x<<endl;
+    cout<<"El valor de y es: "<<y<<endl;
+    cout<<"El valor almacenado en letra es: "<<letra<<endl;
+    //MOSTRAR VALOR ASCII
+    letra = 70;
+    cout<<"El caracter ASCII almacenado en letra es: "<<letra<<endl;
+    //ASIGNAR VALOR A VARIABLE FLOTANTE
+    calificacion1=8.5;
+    calificacion2=9.2;//VISUALIAR VALOR DE VARIABLE
+    nombre = "Oscar";
+    cout<<"El valor asignado a calificacion1 es: "<<calificacion1<<endl;
+    cout<<"El valor asignado a calificacion2 es: "<<calificacion2<<endl;
+
+
+
+
+
     return 0;
 }
