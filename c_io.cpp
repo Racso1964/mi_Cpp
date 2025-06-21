1d42448fedefddfb7b72b8dbdab31c66b9d26430
Racso1964 <oscarrodolfoeo@gmail.com>
2025-06-21 17:47:24 -0400

    Compila: Rompe la corrida al teclear numero no compatible con la variable

--- c_io.cpp
+++ c_io.cpp
@@ -8,13 +8,13 @@ int main()
     int x,y;
     float calificacion1,calificacion2,promedio;
 
-    cout<<"Teclee el valor de x";
+    cout<<"Teclee el valor de x ";
     cin>>x;
-    cout<<"Teclee el valor de y";
+    cout<<"Teclee el valor de y ";
     cin>>y;
 
 
-    cout<<"El numero guardado en x es: "<<x<<endl;
+    cout<<"El numero guardado en x es: "<<x<<endl;// Si algun numero se teclea decimal. el compilador lo lee, lo despliega y se rompe la corrida del programa
 
     
     cout<<"El numero guardado en y es: "<<y<<endl;
