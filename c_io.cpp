22df631708bccd25b4343f246bd755d7d54ff670
Racso1964 <oscarrodolfoeo@gmail.com>
2025-06-21 15:13:56 -0400

    Compila: Pide un dato por teclado

--- c_io.cpp
+++ c_io.cpp
@@ -4,7 +4,18 @@ using namespace std;
 
 int main()
 {
-    
+    //DECLARACION DE VARIABLE
+    int x,y;
+    float calificacion1,calificacion2,promedio;
+
+    cin>>x;
+    cout<<"El numero guardado en x es: "<<x<<endl;
+   
+
+
+
+
+
 
 
 
