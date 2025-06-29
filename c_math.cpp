2316dc81d340bd53fff76dc0ffd2b5db194178fd
Racso1964 <oscarrodolfoeo@gmail.com>
2025-06-28 20:32:09 -0400

    Compila: Crea una funcion

--- c_math.cpp
+++ c_math.cpp
@@ -1,31 +1,19 @@
 #include <iostream>
+#include <cmath>
 
 using namespace std;
 
 int main()
 {
-    float m,l,a,r;
+    //calcular y es igual a equis cuadrado mas 5 equis.
+    float y,x;
+    cout<<"Introduzca el valor de x: ";
+    cin>>x;
+    y= x*x+5*x;
 
-    /*m=10.4;
-    l=125;
-    a=500;
-    r=m*l/a;
-    */
-
-    //Ususario ahora debe intriducir datos por teclado
-
-    cout<<"PROGRAMA QUE CALCULA LA RESISTENCIA ELECTRICA DE UN METAL"<<endl;
-    cout<<"*********************************************************"<<endl;
-
-    cout<<"Introduzca el valor de la resistividad  (m): ";
-    cin>>m;
-    cout<<"Introduzca la longitud del metal  (l): ";
-    cin>>l;
-    cout<<"Introduzca el area del metal  (a): ";
-    cin>>a;
-    r=m*l/a;
-
-    cout<<"La resistencia electrica del alambre es: "<<r<<endl;
+    cout<<"y es igual a: "<<y<<endl;
+    
+    
 
 
 
