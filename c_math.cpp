c18bf3543fa95f33e24b355de345269d88d9a604
Racso1964 <oscarrodolfoeo@gmail.com>
2025-06-29 18:52:53 -0400

    Docs: valor nan de una expresion cuadratica

--- c_math.cpp
+++ c_math.cpp
@@ -5,20 +5,32 @@ using namespace std;
 
 int main()
 {
-    float x,y,z;
+    float x1,x2,z;
+    float a,b,c;
 
-    // z = la raiz cubica de x mas equis al cuadrado mas y al cuadrado; todo ello sobre 5x
+    // Calcular las raices de una ecuacion "ax^²+bx+c=0"
 
-    cout<<"Introduzca el valor de x: ";
-    cin>>x;
+    cout<<"Introduzca el valor de a: ";
+    cin>>a;
 
-    cout<<"Introduzca el valor de y: ";
-    cin>>y;
+    cout<<"Introduzca el valor de b: ";
+    cin>>b;
+
+    cout<<"Introduzca el valor de c: ";
+    cin>>c;
+
+    x1= (-b+sqrt(pow(b,2)-(4*a*c)))/(2*a);
+    x2= (-b-sqrt(pow(b,2)-(4*a*c)))/(2*a);
+
+    cout<<"x1 = "<<x1<<endl;
+    cout<<"x2 = "<<x2<<endl;
+
+    cout<<"comprobacion con x1: "<<(a*(x1*x1)+b*x1+c)<<endl;
+    cout<<"comprobacion con x2: "<<(a*(x2*x2)+b*x2+c);
 
 
-    z=(pow(x,1/3)) + ((pow(x,2))+pow(y,2)) / (5*x);
-    cout<<"z = "<<z<<endl;
-    
+
+   
     
 
 
