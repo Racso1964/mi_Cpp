622439b29cf041fee9685ff07bfbc701994a0fe5
Racso1964 <oscarrodolfoeo@gmail.com>
2025-06-29 11:49:07 -0400

    Compila: Operciones algebraicas

--- c_math.cpp
+++ c_math.cpp
@@ -5,14 +5,18 @@ using namespace std;
 
 int main()
 {
-    //calcular y es igual a raiz cuadrada de 3 equis cuadrada mas 6 sobre 2 equis
-    float y,x;
+    // y = a raiz cuadrada de 3x elevada a 5 sobre 2x+y; todo ello
+    // mas y elevada a la potencia 2 entre x+y
+    float y,x,z;
 
     cout<<"Introduzca el valor de x: ";
     cin>>x;
 
-    y=sqrt((3*pow(x,2)+6)/(2*x));
-    cout<<"y = "<<y<<endl;
+    cout<<"Introduzca el valor de y: ";
+    cin>>y;
+
+    y=sqrt(((3*pow(x,5))/(2*x*y))+((pow(y,2))/(x+y)));
+    cout<<"z = "<<z<<endl;
     
     
 
