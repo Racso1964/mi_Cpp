204b5dbcf617dfd4217d5d898105f14bd57cf00d
Racso1964 <oscarrodolfoeo@gmail.com>
2025-06-29 13:16:11 -0400

    Compila

--- c_math.cpp
+++ c_math.cpp
@@ -5,18 +5,16 @@ using namespace std;
 
 int main()
 {
-    // y = a raiz cuadrada de 3x elevada a 5 sobre 2x+y; todo ello
-    // mas y elevada a la potencia 2 entre x+y
-    float y,x,z;
+    float x,y;
+    
+    // y = la (raiz cuadrada de 3x elevada a 2) mas 6; to do ello sobre 2x
 
     cout<<"Introduzca el valor de x: ";
     cin>>x;
 
-    cout<<"Introduzca el valor de y: ";
-    cin>>y;
 
-    y=sqrt(((3*pow(x,5))/(2*x*y))+((pow(y,2))/(x+y)));
-    cout<<"z = "<<z<<endl;
+    y=sqrt((3*pow(x,2)+6)/(2*x));
+    cout<<"y = "<<y<<endl;
     
     
 
