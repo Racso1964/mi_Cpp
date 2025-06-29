1b1a058d010ea339726c2e56d20c14de35df61fd
Racso1964 <oscarrodolfoeo@gmail.com>
2025-06-29 11:18:34 -0400

    Optimiza el codigo suprimiendo variables auxiliares

--- c_math.cpp
+++ c_math.cpp
@@ -7,13 +7,11 @@ int main()
 {
     //calcular y es igual a raiz cuadrada de 3 equis cuadrada mas 6 sobre 2 equis
     float y,x;
-    float numerador, denominador;
 
     cout<<"Introduzca el valor de x: ";
     cin>>x;
-    numerador=3*pow(x,2)+6;
-    denominador=2*x;
-    y=sqrt(numerador/denominador);
+
+    y=sqrt((3*pow(x,2)+6)/(2*x));
     cout<<"y = "<<y<<endl;
     
     
