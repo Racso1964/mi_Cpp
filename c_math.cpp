91e71f5b1251556b38391bb0b772aba544afddb0
Racso1964 <oscarrodolfoeo@gmail.com>
2025-06-29 19:28:56 -0400

    Compila: Resuelve angulos en redianes

--- c_math.cpp
+++ c_math.cpp
@@ -5,33 +5,17 @@ using namespace std;
 
 int main()
 {
-    float x1,x2,z;
-    float a,b,c;
+    float angulo,y;
 
-    // Calcular las raices de una ecuacion "ax^²+bx+c=0"
+    // Calcular seno de un angulo"
 
-    cout<<"Introduzca el valor de a: ";
-    cin>>a;
+    cout<<"Introduzca el valor del angulo en radianes: ";
+    cin>>angulo;
 
-    cout<<"Introduzca el valor de b: ";
-    cin>>b;
+    y = sin(angulo);
 
-    cout<<"Introduzca el valor de c: ";
-    cin>>c;
+    cout<<"El seno de "<<angulo<<" es: "<<y<<endl;
 
-    x1= (-b+sqrt(pow(b,2)-(4*a*c)))/(2*a);
-    x2= (-b-sqrt(pow(b,2)-(4*a*c)))/(2*a);
-
-    cout<<"x1 = "<<x1<<endl;
-    cout<<"x2 = "<<x2<<endl;
-
-    cout<<"comprobacion con x1: "<<(a*(x1*x1)+b*x1+c)<<endl;
-    cout<<"comprobacion con x2: "<<(a*(x2*x2)+b*x2+c);
-
-
-
-   
-    
 
 
 
