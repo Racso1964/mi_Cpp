9437247ab248316aa7cf71b46000823365beeabb
Racso1964 <oscarrodolfoeo@gmail.com>
2025-06-29 14:04:01 -0400

    Compila :Resuelve raiz cuadrada de expresion

--- c_math.cpp
+++ c_math.cpp
@@ -5,16 +5,19 @@ using namespace std;
 
 int main()
 {
-    float x,y;
-    
-    // y = la (raiz cuadrada de 3x elevada a 2) mas 6; to do ello sobre 2x
+    float x,y,z;
+
+    // z = la raiz cuadrada de{[( 3x elevada a 5)  sobre (2xy)] + [(y al cuadrado)sobre (x+y])]}
 
     cout<<"Introduzca el valor de x: ";
     cin>>x;
 
+    cout<<"Introduzca el valor de y: ";
+    cin>>y;
 
-    y=sqrt((3*pow(x,2)+6)/(2*x));
-    cout<<"y = "<<y<<endl;
+
+    z=sqrt(3*pow(x,5)/(2*x*y)+(pow(y,2))/(x+y));
+    cout<<"z = "<<z<<endl;
     
     
 
