85ed6208c289826a6f30059198fccd920eff8170
Racso1964 <oscarrodolfoeo@gmail.com>
2025-07-12 21:45:58 -0400

    Compila:Resuelveexpresion logica y relacional

---
+++ c_r_l.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main()
+//Expresiones logicas y relacionales
+{
+   cout<<"La operacion 3<5 es:"<<(3<5)<<endl;  
+
+
+
+
+    return 0;
+}
