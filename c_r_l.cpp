c34c1e54736d227f76216ab1ba7caeb8c5f8bf98
Racso1964 <oscarrodolfoeo@gmail.com>
2025-07-13 11:11:52 -0400

    Compila: 1 lo acepta como true

--- c_r_l.cpp
+++ c_r_l.cpp
@@ -7,7 +7,7 @@ int main()
 //Expresiones logicas y relacionales
 {
     bool respuesta;
-    respuesta=true;//C++ recooce la instruccion false o true mas siempre va a devolver un 1 o un cero.
+    respuesta=1;//C++ recooce la instruccion false o true mas siempre va a devolver un 1 o un cero.
     cout<<"La operacion 3>5 es:"<<(3>5)<<endl; 
     
     cout<<"La respuesta es: "<<respuesta<<endl;  
