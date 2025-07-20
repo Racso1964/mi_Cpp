0f318978cb2aa3fc683b5f3e273bec8746b97f69
Racso1964 <oscarrodolfoeo@gmail.com>
2025-07-20 12:19:20 -0400

    Compila: Crea programa para inventario de libros con vectores tipo char

--- c_a.cpp
+++ c_a.cpp
@@ -1,19 +1,26 @@
-// Referencia Documental en README_3.md
-// Recorrer vector y desplega sus elementos
+// Desafio Documental en README_3.md
+// crear un programa con el cual podamos guardar los títulos y los autores de diferentes libros sin perder ninguno de ellos
+// El usuario es el encargado de suministrar la información de cada libro.
+// Vamos a suponer que el usuario solo podrá ingresar un máximo de 5 libros, para así tener un tamaño de vector fijo
+
+// Se delcaran los vectores de tipo char
 #include <iostream>
 
 using namespace std;
 
 int main()
 {
-   int edades[]={1,2,9,8,16,32,9,50,36,20,1,87};
-   int limite = (sizeof(edades)/sizeof(edades[0]));
-
-   for(int i=0; i < limite; i++)
-   {
-
-      cout<<edades[i]<<endl;
-   }
+   char titulos[5];
+   char autores[5];
+    cout << "Por favor ingrese la siguiente información de los Libros: \n";
+    for(int i = 0; i < 5; i++)
+      {
+            cout << "\n******* Libro " << i + 1 <<"********:\n";
+            cout << "Titulo: ";
+            cin >> titulos[i];
+            cout << "Autor: ";
+            cin >> autores[i];
+      }
    
     
 
