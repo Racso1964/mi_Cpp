96760bad827bc9b580960ce5d4452ce8a8fcfa2e
Racso1964 <oscarrodolfoeo@gmail.com>
2025-07-20 10:30:10 -0400

    Compila: Determina el tamaño de un vector

--- c_a.cpp
+++ c_a.cpp
@@ -1,10 +1,13 @@
-//Codigo_para_arreglos en C++
+//Codigo_para_arreglos en C++. Referencia Documental en README_3.md
 #include <iostream>
 
 using namespace std;
 
 int main()
 {
+   int edades[]={1,2,9,8,16,32,9,50,36,20,1,87};
+   int limite = (sizeof(edades)/sizeof(edades[0]));
+   cout<<"El tamaño del vector en byte es: "<<limite<<endl;
     
 
    return 0;
