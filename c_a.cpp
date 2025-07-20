2ac0f991abba030c158a2d439555382e9d00b71a
Racso1964 <oscarrodolfoeo@gmail.com>
2025-07-20 12:51:06 -0400

    Compila Implementa vectores tipo string

--- c_a.cpp
+++ c_a.cpp
@@ -3,15 +3,17 @@
 // El usuario es el encargado de suministrar la información de cada libro.
 // Vamos a suponer que el usuario solo podrá ingresar un máximo de 5 libros, para así tener un tamaño de vector fijo
 
-// Se delcaran los vectores de tipo char
+// Se delcaran los vectores de tipo string
+// Las entradas cin al contener espacios generan inconsistencia petitiva
+// Se debe modificar la entrada de texto usando la funcion getline
 #include <iostream>
 
 using namespace std;
 
 int main()
 {
-   char titulos[5];
-   char autores[5];
+   string titulos[5];
+   string autores[5];
     cout << "Por favor ingrese la siguiente información de los Libros: \n";
     for(int i = 0; i < 5; i++)
       {
