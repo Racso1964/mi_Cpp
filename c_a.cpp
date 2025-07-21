7260f6f09421103674279c03898407b565c8d8f1
Racso1964 <oscarrodolfoeo@gmail.com>
2025-07-20 20:23:36 -0400

    Compila: Evalua proposicion con el oporador ||

--- c_a.cpp
+++ c_a.cpp
@@ -8,8 +8,11 @@ int main(){
    float calificacion;
    short int faltas;
 
+   faltas = 15;
    calificacion = 8.5;
    cout<<"Rango aprobatorio: "<<((calificacion >= 7) and (calificacion <= 10))<<endl;
+
+   cout<<"Si la calificacion "<<(calificacion <= 7 || faltas > 10);
 
  
    
