c98c6ff2f225319fcae528d3f35118c160fbc3c4
Racso1964 <oscarrodolfoeo@gmail.com>
2025-07-20 20:35:40 -0400

    Compila: Evalua condicionales multiples

--- c_a.cpp
+++ c_a.cpp
@@ -10,9 +10,9 @@ int main(){
 
    faltas = 15;
    calificacion = 8.5;
-   cout<<"Rango aprobatorio: "<<((calificacion >= 7) and (calificacion <= 10))<<endl;
+   cout<<"aprobado: "<<((calificacion >= 7) and (calificacion <= 10) && (faltas <= 10))<<endl;
 
-   cout<<"Si la calificacion "<<(calificacion <= 7 || faltas > 10);
+   cout<<"reprobado "<<(calificacion <= 7 || faltas > 10);
 
  
    
