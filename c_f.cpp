ec519b7066d020485b90294c7792092ead5b9319
Racso1964 <oscarrodolfoeo@gmail.com>
2025-07-21 21:55:49 -0400

    Compila: Crea funcion que no devuelve dato resultado pero si recibe argumentos

--- c_f.cpp
+++ c_f.cpp
@@ -2,9 +2,10 @@
 #include <iostream>
 
 using namespace std;
-// función que no recibe argumento y no devuelve argumento
+// función que si recibe argumento y no devuelve argumento
+// Recibe dos numeros, los compara y muestra en consola cual es mayor
 
-void portada()
+void portada() //Funcion que muetra texto y crea portada
 {
     cout<<"¡ BIENVENIDOS !"<<endl;
     cout<<"PROGRAMA PARA DEMOSTRAR EL USO DE FUNCIONES"<<endl;
@@ -12,9 +13,38 @@ void portada()
         cout<<"*";
 }
 
-int main()
+void compara(int a, int b)
 {
-    portada();
+    cout<<"A: "<<a<<endl;
+    cout<<"B: "<<b<<endl;
+
+    if (a >= b)
+        cout<<"\n A es mayor"<<endl;
+    else
+        cout<<"\n B es mayor"<<endl;
+
+}
+
+
+
+int main()
+
+{
+    int x,y;
+
+    portada();// llamada a la funcion portada
+
+    cout<<"\n Introduzca un numero: ";// Prepara argumento para enviar a la funcion "compara"
+    cin>>x;
+
+    cout<<"\n Introduzca un segundo numero: ";// Prepara argumento para enviar a la funcion "compara"
+    cin>>y;
+
+    compara(x,y);//llamada a la funcion compara
+
+    
+
+
 
    return 0;
 }     
