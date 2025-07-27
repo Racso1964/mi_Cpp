5bc2ac6bbc5e13b71e0df7e926bf557fcd0e3a2d
Racso1964 <oscarrodolfoeo@gmail.com>
2025-07-27 11:22:41 -0400

    Compila: Declaracion de la funcion que recibira como argumento el arreglo

--- c_f_1.cpp
+++ c_f_1.cpp
@@ -2,7 +2,11 @@
 
 using namespace std;
 
-//Programa crea arreglo de 1 dimension y carga sus datos
+// -Programa crea arreglo de 1 dimension y carga sus datos
+// -Declara una funcion que contendra la declaracion de un arreglo 
+//  y a su vez esta funcion devolver un valor flotante de la media de los datos del vector
+
+float calcula_media(); // QUINTO: Declaracion de la funcion para calcular la media
 
 int main(){
     short int n,i; //PRIMERO: Declara variables: "n" para numero de lelementos del vector, "i" para ciclo for
@@ -14,6 +18,8 @@ int main(){
     cout<<"Introduzca los datos del vector  \n"; //TERCERO
     for(i=0; i<n; i++) //Ciclo for para pedir los elementos del arreglo
         cin>>A[i]; //CUARTO: Pedimos cada elemento del arreglo "A" en la posicion [i]
+    
+    
     
 
 
